04dc39ceaa0698fd916e743d2984d1e72a8c3b23
Piotr <113837020+TheOtherLword@users.noreply.github.com>
2022-11-16 21:02:54 +0100

    another part

--- SimpleStoreOperationsAutomatic/src/test/java/page/MainPage.java
+++ SimpleStoreOperationsAutomatic/src/test/java/page/MainPage.java
@@ -11,6 +11,9 @@ public class MainPage {
     @FindBy(css = ".user-info > a:nth-child(1) > span:nth-child(2)")
     private WebElement signInBtn;
 
+    @FindBy(css = "div.js-product:nth-child(2) > article:nth-child(1) > div:nth-child(1) > div:nth-child(1) > a:nth-child(1) > img:nth-child(1)")
+    private WebElement Item;
+
     public MainPage(WebDriver driver){
         this.driver = driver;
         PageFactory.initElements(driver,this);
@@ -19,4 +22,10 @@ public class MainPage {
         signInBtn.click();
         return new MainPage(driver);
     }
+
+    public MainPage goToItemPage(){
+        Item.click();
+        return new MainPage(driver);
+    }
+
 }
